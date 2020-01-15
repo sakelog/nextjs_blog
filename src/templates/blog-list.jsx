524df284996218daeaddc7ef286db1be5bca9698
sake <sak.kikuchi@gmail.com>
2020-01-15 21:59:51 +0900

    投稿日表示修正

--- src/templates/blog-list.jsx
+++ src/templates/blog-list.jsx
@@ -1,60 +1,70 @@
 import React from "react"
-import { graphql } from "gatsby"
-import { Link } from "gatsby"
+import { graphql,Link } from "gatsby"
 import Layout from "../components/layout"
 import Head from "../components/head"
 import Pagination from "../components/pagination"
+import { FiClock } from "react-icons/fi";
+import PropTypes from "prop-types"
 
 const _ = require("lodash")
+const pageTitle = "記事一覧"
 
-export default class BlogList extends React.Component {
-    render() {
-        const posts = this.props.data.allMarkdownRemark.edges
-        const { currentPage, numPages } = this.props.pageContext
+const BlogList = ({pageContext, data}) => {
+  const posts = data.allMarkdownRemark.edges
+  const { currentPage, numPages } = pageContext
 
-        return (
-            <Layout>
-              <Head />
-              <h2 className="">
-                <span>記事一覧</span>
-              </h2>
-              {posts.map(({ node }) => {
-                  const title = node.frontmatter.title || node.fields.slug
-                  const description = node.frontmatter.description || node.excerpt
-                  const categoryPath = `/category/${_.kebabCase(node.frontmatter.category)}/`
+  return(
+    <Layout>
+      <Head
+        title={pageTitle}
+        description={`sakelogの記事一覧ページ：${currentPage}`}
+      /> 
+      <h2>{pageTitle}</h2>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        const description = node.frontmatter.description || node.excerpt
+        const categoryPath = `/category/${_.kebabCase(node.frontmatter.category)}/`
 
-                  const Tags = node.frontmatter.tags
-                  const tag_list = Tags.map((tag) =>
-                    <li key={tag} className="list-inline-item">
-                      <Link to={`/tags/${_.kebabCase(tag)}/`}>
-                        <h5 className="cats"><span className="badge badge-secondary">#{tag}</span></h5>
-                      </Link>
-                    </li>
-                  )
-                  
-              return (
-                <div key={node.fields.slug} className="border-bottom pt-3 px-2">
-                  <small>投稿日：{node.frontmatter.date}</small>
-                  <h3><Link to={node.fields.slug}>
-                    {title}
-                  </Link></h3>
-                  <ul className="list-inline">
-                    <li className="list-inline-item">
-                      <Link to={categoryPath}>
-                        <h4 className="cats"><span className="badge badge-primary">{node.frontmatter.category}</span></h4>
-                      </Link>
-                    </li>
-                    {tag_list}
-                  </ul>
-                  <p>{description}</p>
-                </div>
-              )
-              })}
-              <Pagination numPages={numPages} currentPage={currentPage} />
-            </Layout>
+        const Tags = node.frontmatter.tags
+        const tag_list = Tags.map((tag) =>
+          <li key={tag} className="list-inline-item">
+            <Link to={`/tags/${_.kebabCase(tag)}/`}>
+              <h5 className="cats">#{tag}</h5>
+            </Link>
+          </li>
         )
-    }
+        
+        return (
+          <div key={node.fields.slug} className="border-bottom pt-3 px-2">
+            <h3><Link to={node.fields.slug}>
+              {title}
+            </Link></h3>
+            <small><FiClock /><span className="ml-1">{node.frontmatter.date}</span></small>
+            <ul className="list-inline my-2">
+              <li className="list-inline-item">
+                <Link to={categoryPath} className="badge badge-primary">
+                  <h4 className="cats">{node.frontmatter.category}</h4>
+                </Link>
+              </li>
+              {tag_list}
+            </ul>
+            <p>{description}</p>
+          </div>
+        )
+        })}
+        <Pagination numPages={numPages} currentPage={currentPage} />
+    </Layout>
+  )
 }
+
+BlogList.propTypes = {
+  pageContext: PropTypes.shape({
+    currentPage: PropTypes.number.isRequired,
+    numPages: PropTypes.number.isRequired,
+  }),
+}
+
+export default BlogList
 export const blogListQuery = graphql`
 query blogListQuery($skip: Int!, $limit: Int!) {
     allMarkdownRemark(
@@ -70,7 +80,7 @@ query blogListQuery($skip: Int!, $limit: Int!) {
               excerpt
               frontmatter {
                 title
-                date
+                date(formatString: "YYYY/MM/DD")
                 description
                 category
                 tags
--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -2,81 +2,86 @@ import React from "react"
 import { graphql,Link } from "gatsby"
 import Layout from "../components/layout"
 import Head from "../components/head"
+import { FiClock } from "react-icons/fi";
+import PropTypes from "prop-types"
 // Utilities
 import kebabCase from "lodash/kebabCase"
 
-//const _ = require("lodash")
+const BlogPost = ({pageContext, data}) => {
+  const post = data.markdownRemark
+  const { prev, next } = pageContext
 
-class BlogPostTemplate extends React.Component {
-  render(){
-    const post = this.props.data.markdownRemark
-    const { prev, next } = this.props.pageContext
+  const categoryPath = `/category/${kebabCase(post.frontmatter.category)}/`
 
-    const categoryPath = `/category/${kebabCase(post.frontmatter.category)}/`
+  const Tags = post.frontmatter.tags
+  const tag_list = Tags.map((tag) =>
+    <li key={tag} className="list-inline-item">
+      <Link to={`/tags/${kebabCase(tag)}/`}>
+        <h5 className="cats">#{tag}</h5>
+      </Link>
+    </li>
+  )
 
-    const Tags = post.frontmatter.tags
-    const tag_list = Tags.map((tag) =>
-      <li key={tag} className="list-inline-item">
-        <Link to={`/tags/${kebabCase(tag)}/`}>
-          <h5 className="cats"><span className="badge badge-secondary">#{tag}</span></h5>
+  return(
+    <Layout>
+    <Head
+      title={post.frontmatter.title}
+      description={post.frontmatter.description}
+    />
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <Link to={categoryPath} className="badge badge-primary my-2">
+          <h4 className="cats">{post.frontmatter.category}</h4>
         </Link>
-      </li>
-    )
-
-    return(
-      <Layout>
-      <Head
-        title={post.frontmatter.title}
-        description={post.frontmatter.description}
-      />
-        <div>
-          <h1>{post.frontmatter.title}</h1>
-          <Link to={categoryPath}>
-            <h4 className="cats"><span className="badge badge-primary">{post.frontmatter.category}</span></h4>
-          </Link>
-          <p className="text-muted">投稿日：{post.frontmatter.date}</p>
-          <hr />
-
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-
-          <ul className="list-inline"><li className="list-inline-item">タグ：</li>{tag_list}</ul>
-
-        </div>
+        <p className="text-muted"><FiClock />投稿日：{post.frontmatter.date}</p>
         <hr />
-        <nav>
-        <div className="row">
+
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+
+        <ul className="list-inline"><li className="list-inline-item">タグ：</li>{tag_list}</ul>
+
+      </div>
+      <hr />
+      <nav>
+      <div className="row">
+          <div className="col-sm mx-3">
+              {prev && (
+                <div className="d-flex justify-content-start row">
+                  <div>前：</div>
+                  <div className="col-10">
+                    <Link to={prev.fields.slug} rel="prev">
+                      {prev.frontmatter.title}
+                    </Link>
+                  </div>
+                </div>
+              )}
+            </div>
             <div className="col-sm mx-3">
-                {prev && (
-                  <div className="d-flex justify-content-start row">
-                    <div>前：</div>
-                    <div className="col-10">
-                      <Link to={prev.fields.slug} rel="prev">
-                        {prev.frontmatter.title}
-                      </Link>
-                    </div>
+              {next && (
+                <div className="d-flex justify-content-end row">
+                  <div className="col-10 text-right">
+                    <Link to={next.fields.slug} rel="next">
+                      {next.frontmatter.title} 
+                    </Link>
                   </div>
-                )}
-              </div>
-              <div className="col-sm mx-3">
-                {next && (
-                  <div className="d-flex justify-content-end row">
-                    <div className="col-10 text-right">
-                      <Link to={next.fields.slug} rel="next">
-                        {next.frontmatter.title} 
-                      </Link>
-                    </div>
-                    <div>：次</div>
-                  </div>
-                )}
-              </div>
-          </div>
-        </nav>
-      </Layout>
-    )
-  }
+                  <div>：次</div>
+                </div>
+              )}
+            </div>
+        </div>
+      </nav>
+    </Layout>
+  )
 }
 
-export default BlogPostTemplate
+BlogPost.propTypes = {
+  pageContext: PropTypes.shape({
+    prev: PropTypes.element.isRequired,
+    next: PropTypes.element.isRequired,
+  }),
+}
+
+export default BlogPost
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -90,7 +95,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date
+        date(formatString: "YYYY/MM/DD")
         category
         tags
       }
