81174ac75d7c6540ee2b6db23c603decb8613fa2
sake <sak.kikuchi@gmail.com>
2020-01-16 22:59:08 +0900

    PWA対応

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,59 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-lodash`,
     {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'sake log',
+        short_name: 'sake log',
+        start_url: '/?source=homescreen',
+        background_color: '#333',
+        theme_color: '#164a84',
+        display: 'minimal-ui',
+        "icons": [
+          {
+            "src": "icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `post`,
