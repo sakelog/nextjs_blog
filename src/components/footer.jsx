e4a14da720264742bd2339f6abbc80f404605b36
sake <sak.kikuchi@gmail.com>
2020-01-19 15:40:26 +0900

    footer見た目修正・GitHubへのリンク追加

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+// Icons
+import { GoMarkGithub } from "react-icons/go";
+
 
 const Footer = () => {
   const data = useStaticQuery(
@@ -16,8 +19,15 @@ const Footer = () => {
   )
   const SiteTitle = data.site.siteMetadata.title
   return(
-  <footer className="bg-dark text-light text-center p-2 mt-auto">
-    &copy;2020 {SiteTitle}
+  <footer className="bg-secondary text-light p-2 mt-auto d-flex flex-column flex-md-row">
+    <div className="w-100 text-center">
+      &copy;2020 {SiteTitle}
+    </div>
+    <div className="flex-shrink-1 text-center px-4 footer-icon">
+      <a href="https://github.com/sakelog" target="_blank" rel="noopener noreferrer" className="text-white">
+        <GoMarkGithub />
+      </a>
+    </div>
   </footer>
   )
 }
