4970ca4f45d8f8ebe9c7cd421108c25712869b0e
sake <sakelog.website@gmail.com>
2020-01-20 21:38:18 +0900

    バグ修正

--- src/components/header-cat-list.tsx
+++ src/components/header-cat-list.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
 // Utilities
-import kebabCase from "lodash/kebabCase"
+import { kebabCase } from "lodash"
 
 const HeaderCatList = () => {
   const data = useStaticQuery(
@@ -20,8 +20,9 @@ const HeaderCatList = () => {
   const categorys = data.allMarkdownRemark.group
   return(
   <div>
-    {categorys.map((category: { fieldValue: {} }) => (
+    {categorys.map((category: { fieldValue: string },index:number) => (
         <Link
+          key={index}
           to={`/category/${kebabCase(category.fieldValue)}/`}
           className="btn btn-outline-light mx-1"
         >
