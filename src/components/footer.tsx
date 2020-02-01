460f29c8855616dc49e773e7d327d36f6074d6e4
sake <sakelog.website@gmail.com>
2020-02-01 20:20:04 +0900

    アクセシビリティ改善

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -19,12 +19,12 @@ const Footer = () => {
   )
   const SiteTitle = data.site.siteMetadata.title
   return(
-  <footer className="bg-secondary text-light p-2 mt-auto d-flex flex-column flex-md-row">
+  <footer className="bg-light text-dark p-2 mt-auto d-flex flex-column flex-md-row">
     <div className="w-100 text-center">
       &copy;2020 {SiteTitle}
     </div>
     <div className="flex-shrink-1 text-center px-4 footer-icon">
-      <a href="https://github.com/sakelog" target="_blank" rel="noopener noreferrer" className="text-white">
+      <a href="https://github.com/sakelog" target="_blank" rel="noopener noreferrer" role="presentation" aria-label="Github">
         <GoMarkGithub />
       </a>
     </div>
