f3059dcc6cc34fa5cb27a13614f4c092b60c9736
sake <sakelog.website@gmail.com>
2020-06-03 05:48:44 +0900

    デザイン修正

--- src/components/prev-next.tsx
+++ src/components/prev-next.tsx
@@ -22,35 +22,39 @@ interface PrevNextType {
 
 const PrevNext = ({ prev, next }:PrevNextType) => {
   return (
-    <nav className="container">
-      <div className="row">
-        <div className="col-1">
-          {prev && (
-            <div className="pagination-pn">
-              <span>前</span>
-            </div>
-          )}
+    <nav>
+      <div className="grid-2-middle_sm-1">
+        <div className="col-6_sm-12 grid">
+          <div className="col-2">
+            {prev && (
+              <div className="pagination-pn">
+                <span>前</span>
+              </div>
+            )}
+          </div>
+          <div className="col-10">
+            {prev && (
+              <Link to={prev.fields.slug} rel="prev">
+                {prev.frontmatter.title}
+              </Link>
+            )}
+          </div>
         </div>
-        <div className="col-4">
-          {prev && (
-            <Link to={prev.fields.slug} rel="prev">
-              {prev.frontmatter.title}
-            </Link>
-          )}
-        </div>
-        <div className="col-4">
-          {next && (
-            <Link to={next.fields.slug} rel="next">
-              {next.frontmatter.title}
-            </Link>
-          )}
-        </div>
-        <div className="col-1">
-          {next && (
-            <div className="pagination-pn">
-              <span>次</span>
-            </div>
-          )}
+        <div className="col-6_sm-12 grid-right">
+          <div className="col-10">
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title}
+              </Link>
+            )}
+          </div>
+          <div className="col-2">
+            {next && (
+              <div className="pagination-pn">
+                <span>次</span>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </nav>
