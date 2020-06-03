75118ed132948920e12af9fc579865e7ac7dadc3
sake <sakelog.website@gmail.com>
2020-06-04 01:01:27 +0900

    デザイン修正

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,48 +20,41 @@ const Header = () => {
   const SiteTitle = data.site.siteMetadata.title
   return(
     <nav
-      className="navbar navbar-expand-lg navbar-dark bg-primary"
+      className="sl-nav-bar"
       role="navigation"
       aria-label="main navigation"
     >
-      <Link to="/" className="navbar-brand">
-        {SiteTitle}
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#nav-menu"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="nav-menu">
-        <HeaderCatList />
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/tags/" className="nav-link">
-              #タグ一覧
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about_this_site/" className="nav-link">
-              このサイトについて
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/privacy/" className="nav-link">
-              プライバシーポリシー
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact/" className="nav-link">
-              お問い合わせ
-            </Link>
-          </li>
-        </ul>
+      <div className="grid">
+        <div className="col-2_md-12 sl-nav-title-wrapper">
+          <Link to="/" className="sl-nav-title">
+            <span>{SiteTitle}</span>
+          </Link>
+        </div>
+        <div className="col-10_md-12 sl-nav-menu">
+          <HeaderCatList />
+          <div className="grid-4-spaceBetween">
+            <div className="col_sm-12">
+              <Link to="/tags/" className="nav-link">
+                #タグ一覧
+              </Link>
+            </div>
+            <div className="col_sm-12">
+              <Link to="/about_this_site/" className="nav-link">
+                このサイトについて
+              </Link>
+            </div>
+            <div className="col_sm-12">
+              <Link to="/privacy/" className="nav-link">
+                プライバシーポリシー
+              </Link>
+            </div>
+            <div className="col_sm-12">
+              <Link to="/contact/" className="nav-link">
+                お問い合わせ
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   )
