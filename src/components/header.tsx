20850e48f50fc2ae3458bb7a0f2ed64a5b84c0fd
sake <sakelog.website@gmail.com>
2020-06-04 23:44:10 +0900

    スマホメニュー作成

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,8 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 //Components
 import HeaderCatList from "./header-cat-list"
 
+import { FiPlus } from 'react-icons/fi';
+
 const Header = () => {
   const data = useStaticQuery(
     graphql`
@@ -24,36 +26,52 @@ const Header = () => {
       role="navigation"
       aria-label="main navigation"
     >
-      <div className="grid">
+      <div className="grid-noGutter">
         <div className="col-2_md-12 sl-nav-title-wrapper">
           <Link to="/" className="sl-nav-title">
             <span>{SiteTitle}</span>
           </Link>
         </div>
         <div className="col-10_md-12 sl-nav-menu">
-          <HeaderCatList />
-          <div className="grid-4-spaceBetween">
-            <div className="col_sm-12">
-              <Link to="/tags/" className="nav-link">
-                #タグ一覧
-              </Link>
+
+          <div id="nav-drawer">
+
+            <input id="nav-input" type="checkbox" className="nav-unshown" />
+            <div className="nav-open-wrapper">
+              <label id="nav-open" className="nav-unshown" htmlFor="nav-input">
+                <span><FiPlus /></span>
+              </label>
             </div>
-            <div className="col_sm-12">
-              <Link to="/about_this_site/" className="nav-link">
-                このサイトについて
-              </Link>
+
+            <div id="nav-content">
+              <HeaderCatList />
+              <div className="grid-4-spaceBetween">
+                <div className="col_sm-12">
+                  <Link to="/tags/" className="nav-link">
+                    #タグ一覧
+                  </Link>
+                </div>
+                <div className="col_sm-12">
+                  <Link to="/about_this_site/" className="nav-link">
+                    このサイトについて
+                  </Link>
+                </div>
+                <div className="col_sm-12">
+                  <Link to="/privacy/" className="nav-link">
+                    プライバシーポリシー
+                  </Link>
+                </div>
+                <div className="col_sm-12">
+                  <Link to="/contact/" className="nav-link">
+                    お問い合わせ
+                  </Link>
+                </div>
+              </div>
+
             </div>
-            <div className="col_sm-12">
-              <Link to="/privacy/" className="nav-link">
-                プライバシーポリシー
-              </Link>
-            </div>
-            <div className="col_sm-12">
-              <Link to="/contact/" className="nav-link">
-                お問い合わせ
-              </Link>
-            </div>
+
           </div>
+
         </div>
       </div>
     </nav>
