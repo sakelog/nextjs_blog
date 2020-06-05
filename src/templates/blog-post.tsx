4a684bbd039c3b4c0ae51596c619ab636b2498a8
sake <sakelog.website@gmail.com>
2020-06-06 02:31:24 +0900

    blog-post修正

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,51 +1,51 @@
-import * as React from "react"
-import { graphql, Link } from "gatsby"
-import { TempBlogPostQuery } from "../../types/graphql-types"
+import * as React from 'react'
+import { graphql, Link } from 'gatsby'
+import { TempBlogPostQuery } from '../../types/graphql-types'
 
 // Components
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import RenderAst from "../components/renderAst"
-import PrevNext from "../components/prev-next"
-import Bio from "../components/bio"
-import BackToTopPage from "../components/back-to-top-page"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import RenderAst from '../components/renderAst'
+import PrevNext from '../components/prev-next'
+import Bio from '../components/bio'
+import BackToTopPage from '../components/back-to-top-page'
 
 // Utilities
-const kebabCase = require("lodash/kebabCase")
+const kebabCase = require('lodash/kebabCase')
 
 // Icon
-import { FiTag } from 'react-icons/fi';
+import { FiTag } from 'react-icons/fi'
 
 interface BlogPostTypes {
-  pageContext:{
-    prev?:{
-      fields:{
-        slug:string,
-      },
-      frontmatter:{
-        title:string,
-      },
-    },
-    next?:{
-      fields:{
-        slug:string,
-      },
-      frontmatter:{
-        title:string,
-      },
+  pageContext: {
+    prev?: {
+      fields: {
+        slug: string
+      }
+      frontmatter: {
+        title: string
+      }
     }
-  },
-  data:TempBlogPostQuery
+    next?: {
+      fields: {
+        slug: string
+      }
+      frontmatter: {
+        title: string
+      }
+    }
+  }
+  data: TempBlogPostQuery
 }
 
-const BlogPost = ({ pageContext, data }:BlogPostTypes) => {
+const BlogPost = ({ pageContext, data }: BlogPostTypes) => {
   const post = data.markdownRemark
   const { prev, next } = pageContext
 
   const categoryPath = `/category/${kebabCase(post.frontmatter.category)}/`
 
   const Tags = post.frontmatter.tags
-  const tag_list = Tags.map((tag: {},index:number) => (
+  const tag_list = Tags.map((tag: {}, index: number) => (
     <li key={index} className="">
       <Link to={`/tags/${kebabCase(tag)}/`}>
         <h5 className="cats">#{tag}</h5>
@@ -55,17 +55,13 @@ const BlogPost = ({ pageContext, data }:BlogPostTypes) => {
 
   return (
     <Layout>
-      {SEO
-        (post.frontmatter.title,
-        post.frontmatter.description,
-        true)
-      }
+      {SEO(post.frontmatter.title, post.frontmatter.description, true)}
       <div className="Article">
-        <small>{post.frontmatter.date}</small>
-        <Link to={categoryPath} className="sl-cat-badge">
-          <h4 className="cats">{post.frontmatter.category}</h4>
-        </Link>
         <h1>{post.frontmatter.title}</h1>
+        <Link to={categoryPath} className="sl-cat-badge">
+          <h4>{post.frontmatter.category}</h4>
+        </Link>
+        <small>{post.frontmatter.date}</small>
         <hr />
 
         <div className="grid-center">
@@ -75,10 +71,14 @@ const BlogPost = ({ pageContext, data }:BlogPostTypes) => {
           </div>
         </div>
 
-        <div><RenderAst{...post.htmlAst} /></div>
+        <div>
+          <RenderAst {...post.htmlAst} />
+        </div>
 
         <ul className="sl-inline-list">
-          <li><FiTag /></li>
+          <li>
+            <FiTag />
+          </li>
           <li className="">タグ：</li>
           {tag_list}
         </ul>
