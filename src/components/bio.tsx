a09255d15aba9a539cb724640e5d3f5d98020a4c
sake <sakelog.website@gmail.com>
2020-06-06 02:31:24 +0900

    blog-post修正

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -23,55 +23,59 @@ const Bio = () => {
   )
   return (
     <div className="grid-center sl-bio-wrapper">
-        <div className="grid col-8_sm-10_xs-12 sl-bio">
-            <div className="col-4_sm-12">
-                <div className="grid-center sl-align-center">
-                    <div className="col-12_sm-8">
-                        <Img fluid={data.profileImg.childImageSharp.fluid} />
-                    </div>
-                    <div className="col-12">
-                        <p className="sl-bio-title">sake</p>
-                    </div>
-                    <div className="col-12">
-                    <Link to="/about_this_site/" className="sl-btn sl-btn-primary">
-                        プロフィール
-                        </Link>
-                    </div>
-                </div>
+      <div className="grid col-8_sm-10_xs-12 sl-bio">
+        <div className="col-4_sm-12">
+          <div className="grid sl-align-center">
+            <div className="col-12_sm-8">
+              <Link to="/about_this_site/">
+                <Img fluid={data.profileImg.childImageSharp.fluid} />
+              </Link>
             </div>
-
-            <div className="grid-4-middle_sm-2 col-8_sm-12">
-                <div className="col-12">
-                    <p>システムエンジニア。普段はホスト系のお仕事してます。</p>
-                </div>
-                <div className="grid-3 col-12">
-                    <div className="col">
-                        <a
-                        href="https://github.com/sakelog"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        role="presentation"
-                        aria-label="Github"
-                        className="sl-icon"
-                        >
-                        <GoMarkGithub />
-                        </a>
-                    </div>
-                    <div className="col">
-                        <a
-                        href="https://twitter.com/sake_engineer"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        role="presentation"
-                        aria-label="Twitter"
-                        className="sl-icon"
-                        >
-                        <AiOutlineTwitter />
-                        </a>
-                    </div>
-                </div>
+            <div className="col-12">
+              <p className="sl-bio-title">sake</p>
             </div>
+          </div>
         </div>
+
+        <div className="col-8_sm-12">
+          <p>
+            現役システムエンジニア。
+            <br />
+            普段はホスト系のお仕事してます。
+            <br />
+            ブログでは主にWeb関係の技術について、勉強したことや実際にコーディングしてみた内容を発信していきます。
+          </p>
+          <Link to="/about_this_site/" className="sl-btn sl-btn-primary">
+            プロフィール
+          </Link>
+          <ul className="sl-inline-list">
+            <li>
+              <a
+                href="https://github.com/sakelog"
+                target="_blank"
+                rel="noopener noreferrer"
+                role="presentation"
+                aria-label="Github"
+                className="sl-icon"
+              >
+                <GoMarkGithub />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/sake_engineer"
+                target="_blank"
+                rel="noopener noreferrer"
+                role="presentation"
+                aria-label="Twitter"
+                className="sl-icon"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
