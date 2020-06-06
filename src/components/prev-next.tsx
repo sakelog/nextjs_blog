aa391a2ccbc7e77f4961b93ed17c637faf1811bb
sake <sakelog.website@gmail.com>
2020-06-06 22:18:40 +0900

    バグ修正

--- src/components/prev-next.tsx
+++ src/components/prev-next.tsx
@@ -25,14 +25,14 @@ const PrevNext = ({ prev, next }:PrevNextType) => {
     <nav>
       <div className="grid-2-middle_sm-1">
         <div className="col-6_sm-12 grid">
-          <div className="col-2">
+          <div className="col-1">
             {prev && (
               <div className="sl-prevnext">
                 <span>前</span>
               </div>
             )}
           </div>
-          <div className="col-10">
+          <div className="col-11 sl-align-left">
             {prev && (
               <Link to={prev.fields.slug} rel="prev">
                 {prev.frontmatter.title}
@@ -41,14 +41,14 @@ const PrevNext = ({ prev, next }:PrevNextType) => {
           </div>
         </div>
         <div className="col-6_sm-12 grid-right">
-          <div className="col-10">
+          <div className="col-11 sl-align-right">
             {next && (
               <Link to={next.fields.slug} rel="next">
                 {next.frontmatter.title}
               </Link>
             )}
           </div>
-          <div className="col-2">
+          <div className="col-1">
             {next && (
               <div className="sl-prevnext">
                 <span>次</span>
