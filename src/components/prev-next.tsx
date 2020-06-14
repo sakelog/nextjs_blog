f653b207dc2fb729ba50e41e8e5ca9848f7d4746
sake <sakelog.website@gmail.com>
2020-06-14 13:03:22 +0900

    デザイン修正

--- src/components/prev-next.tsx
+++ src/components/prev-next.tsx
@@ -1,60 +1,52 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react'
+import { Link } from 'gatsby'
 
 interface PrevNextType {
-  prev?:{
-    fields:{
-      slug:string,
-    },
-    frontmatter:{
-      title:string,
-    },
-  },
-  next?:{
-    fields:{
-      slug:string,
-    },
-    frontmatter:{
-      title:string,
-    },
+  prev?: {
+    fields: {
+      slug: string
+    }
+    frontmatter: {
+      title: string
+    }
+  }
+  next?: {
+    fields: {
+      slug: string
+    }
+    frontmatter: {
+      title: string
+    }
   }
 }
 
-const PrevNext = ({ prev, next }:PrevNextType) => {
+const PrevNext = ({ prev, next }: PrevNextType) => {
   return (
     <nav>
-      <div className="grid-2-middle_sm-1">
-        <div className="col-6_sm-12 grid">
-          <div className="col-1">
-            {prev && (
-              <div className="sl-prevnext">
-                <span>前</span>
-              </div>
-            )}
-          </div>
-          <div className="col-11 sl-align-left">
-            {prev && (
-              <Link to={prev.fields.slug} rel="prev">
-                {prev.frontmatter.title}
-              </Link>
-            )}
-          </div>
+      <div className="sl-prevnext">
+        <div className="sl-prevnext-item sl-prev">
+          {prev && (
+            <div className="sl-prevnext-icon">
+              <span>前</span>
+            </div>
+          )}
+          {prev && (
+            <Link to={prev.fields.slug} rel="prev">
+              {prev.frontmatter.title}
+            </Link>
+          )}
         </div>
-        <div className="col-6_sm-12 grid-right">
-          <div className="col-11 sl-align-right">
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title}
-              </Link>
-            )}
-          </div>
-          <div className="col-1">
-            {next && (
-              <div className="sl-prevnext">
-                <span>次</span>
-              </div>
-            )}
-          </div>
+        <div className="sl-prevnext-item sl-next">
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title}
+            </Link>
+          )}
+          {next && (
+            <div className="sl-prevnext-icon">
+              <span>次</span>
+            </div>
+          )}
         </div>
       </div>
     </nav>
