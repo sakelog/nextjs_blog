4e12d29c76b2bb010175dd7a55c17a8c29250e1f
sake <sakelog.website@gmail.com>
2020-06-26 05:23:52 +0900

    style指定方法修正

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import Header from './header'
 import Footer from './footer'
 
+import '../styles/css/mystyle.css'
+
 const Layout = ({ children }: any) => {
   return (
     <div className="l-wrapper">
