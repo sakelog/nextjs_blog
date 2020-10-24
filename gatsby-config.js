f618a75f0dc3dc152ee406211059b55841a4523b
sake <sakelog.website@gmail.com>
2020-10-24 22:29:55 +0900

    google adsense設定変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,12 @@ module.exports = {
         id: 'GTM-N9SLS2F',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        publisherId: `ca-pub-5013956882447566`,
+      },
+    },
     // develop
     `gatsby-plugin-lodash`,
     `gatsby-plugin-typescript`,
