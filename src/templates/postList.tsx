0eafc17face85b72e0a3fc4d6613641f93a53473
sake <sakelog.website@gmail.com>
2021-01-01 02:32:06 +0900

    refoctoring--postList

--- src/templates/postList.tsx
+++ src/templates/postList.tsx
@@ -1,7 +1,54 @@
 import * as React from 'react';
+import { graphql, Link } from 'gatsby';
+import _ from 'lodash';
 
-const PostList = (pageContext) => {
-  return <div>test</div>;
+const PostList: PostList.func = (props) => {
+  const posts = props.data.posts.nodes;
+
+  const postListTag = posts.map((post) => {
+    const categoryPath = `/category/${_.kebabCase(post.category.slug)}/`;
+    return (
+      <div key={post.slug} className="u-border--bottom u-space--pad--2">
+        {/*<PostDate postdate={node.date} update={node.update} />*/}
+        <h2>
+          <Link to={`/${post.slug}/`}>{post.title}</Link>
+        </h2>
+        <p>{post.description}</p>
+        <Link to={categoryPath} className="c-badge">
+          <h3>{post.category.name}</h3>
+        </Link>
+        {/*<TagList Tags={node.tags} />*/}
+      </div>
+    );
+  });
+
+  return <div>{postListTag}</div>;
 };
 
 export default PostList;
+
+export const pageQuery = graphql`
+  query tempPostList($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(
+      limit: $limit
+      skip: $skip
+      sort: { fields: date, order: DESC }
+    ) {
+      nodes {
+        slug
+        title
+        date
+        update
+        description
+        category {
+          slug
+          name
+        }
+        tags {
+          slug
+          name
+        }
+      }
+    }
+  }
+`;
