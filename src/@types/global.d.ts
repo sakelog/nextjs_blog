c3d2bd2434c58f8533c071c06ecf39189c828ce5
sake <sakelog.website@gmail.com>
2021-01-01 02:32:06 +0900

    refoctoring--postList

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,59 +1,35 @@
 declare namespace PostList {
-  export type pathQuery = {
-    postListQuery: {
-      nodes: [{ slug: string }];
-    };
-  };
-}
-
-/*declare namespace PostList {
-  declare type pathQuery =  {
-    postListQuery? : {
-      nodes: [{
-
-    }]
-  }}
-
   declare type props = {
     pageContext: {
-      currentPage: number;
+      limit: number;
+      skip: number;
       numPages: number;
-    },
-    data : {
-      site: {
-        siteMetadata: {
-          title: string;
-        }
-      }
-      Posts: {
-        edges: [{
-          node: {
-            slug:string;
-            title:string;
-            date:string;
-            update:string;
-            description:string;
+      currentPage: number;
+    }
+    data: {
+      posts: {
+        nodes: [
+          {
+            slug: string;
+            title: string;
+            date: string;
+            update: string;
+            description: string;
             category: {
-              name:string;
-              slug:string;
+              slug: string;
+              name: string;
             }
-            tags: [{
-              name:string;
-              slug:string;
-            }]
+            tags: [
+              {
+                slug: string;
+                name: string;
+              }
+            ]
           }
-        }]
-      }
-      Category: {
-        edges: [{
-          node: {
-            name:string;
-            slug:string;
-          }
-        }]
+        ]
       }
     }
   }
+
   declare type func = (props:props) => JSX.Element
 }
-*/
