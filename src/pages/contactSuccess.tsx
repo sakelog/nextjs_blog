8e5d737d571df332cc5a249cea8910b3b3ca8a0c
sake <sakelog.website@gmail.com>
2021-01-02 18:43:53 +0900

    refoctoring--contact

--- src/pages/contactSuccess.tsx
+++ src/pages/contactSuccess.tsx
@@ -3,23 +3,24 @@ import config from '../components/config';
 
 import Layout from '../components/layout/layout';
 import CustomHead from '../components/customHead';
-import ContactForm from '../components/contactForm';
 import BackToTopPage from '../components/pagination/backToTopPage';
 
 const pageTitle = 'お問い合わせ';
 const description = config.title + 'についてのお問い合わせページ';
 
-const Contact = () => {
+const ContactSuccess = () => {
   return (
     <Layout>
       {CustomHead(pageTitle, description, false)}
-      <section>
-        <h1 className="u-align--center">{pageTitle}</h1>
-        <ContactForm />
+      <section role="alert">
+        <h2 className="">送信成功</h2>
+        <p>
+          お問い合わせ内容は送信されました。確認後、返信させていただきます。
+        </p>
       </section>
       <BackToTopPage />
     </Layout>
   );
 };
 
-export default Contact;
+export default ContactSuccess;
