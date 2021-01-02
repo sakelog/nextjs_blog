ff6356478bb5c2a80f0ad38925c4aaddbcedf363
sake <sakelog.website@gmail.com>
2021-01-02 14:42:40 +0900

    bug-fix

--- src/components/articleParts/taglist.tsx
+++ src/components/articleParts/taglist.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 
-import { getTagPath } from '../../lib/getPath';
-
-//Icon
 import { FiTag } from 'react-icons/fi';
+
+import { getTagPath } from '../../lib/getPath';
 
 const TagList = (tags) => {
   const targetTags: [{ slug: string; name: string }] = tags.tags;
   const items = targetTags
     ? targetTags.map((tag) => {
         return (
-          <li key={tag.slug}>
+          <li key={tag.slug} className="c-tag-item">
             <Link to={getTagPath(tag.slug)}>
               <h5>{tag.name}</h5>
             </Link>
@@ -23,8 +22,8 @@ const TagList = (tags) => {
     <ul className="p-tag-list">
       <li>
         <FiTag />
-        {items}
       </li>
+      {items}
     </ul>
   ) : null;
 
