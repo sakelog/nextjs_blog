8dce4d441ec1d1ec2b6ba79dca3704ee84eb8d17
sake <sakelog.website@gmail.com>
2021-01-29 01:24:15 +0900

    next-offline

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
-const withPWA = require('next-pwa');
+//const withPWA = require('next-pwa');
+const withOffline = require('next-offline');
 
-module.exports = withPWA({
+module.exports = withOffline({
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: ['images.ctfassets.net'],
   },
+  /*
   pwa: {
     dest: 'public',
   },
+  */
 });
