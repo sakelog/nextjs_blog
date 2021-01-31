2d60177f8f2b5c1986e990a154aee9e0effe90df
sake <sakelog.website@gmail.com>
2021-01-31 19:08:33 +0900

    drawer遅延ロード

--- src/layout/_header.tsx
+++ src/layout/_header.tsx
@@ -1,23 +1,28 @@
 import Link from 'next/link';
-import { MdMenu, MdClose } from 'react-icons/md';
-
+import loadable from '@loadable/component';
+import { MdMenu } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
-import state from '@state/ducks/index';
 
+import state from '@state/ducks/index';
 import config from '@component/config';
 
+const Drawer = loadable(() => import('./headerNav/drawer'));
+import styles from '@styles/project/_p-header.module.scss';
+
+/*
 import PageList from '@layout/headerNav/pageList';
 import SocialIcon from '@component/socialIcon';
 
 import styles from '@styles/project/_p-header.module.scss';
 import headingStyles from '@styles/component/_c-heading.module.scss';
-
+*/
 const Header: React.FC = () => {
   const dispatch = useDispatch();
   const drawerState = state.drawerState;
+  //const windowSizeState = state.windowSizeState;
 
   const handleOpen = () => dispatch(drawerState.drawerOperations.open());
-  const handleClose = () => dispatch(drawerState.drawerOperations.close());
+  //const handleClose = () => dispatch(drawerState.drawerOperations.close());
 
   const SiteTitle = <Link href="/">{config.title}</Link>;
   return (
@@ -30,37 +35,7 @@ const Header: React.FC = () => {
               <MdMenu onClick={() => handleOpen()} />
             </span>
           </div>
-          <div
-            className={
-              styles.drawerBack +
-              ' ' +
-              styles[drawerState.drawerSelectors.drawerSelector()]
-            }
-            onClick={() => handleClose()}
-          ></div>
-          <div
-            className={
-              styles.navDrawer +
-              ' ' +
-              styles[drawerState.drawerSelectors.drawerSelector()]
-            }
-          >
-            <span className={styles.navIcon}>
-              <MdClose onClick={() => handleClose()} />
-            </span>
-            <h2 className={styles.heading + ' ' + headingStyles.flexCenter}>
-              ページ一覧
-            </h2>
-            <div className={styles.pageList}>
-              <PageList />
-            </div>
-            <h2 className={styles.heading + ' ' + headingStyles.flexCenter}>
-              連絡先
-            </h2>
-            <div className={styles.socialIcon}>
-              <SocialIcon />
-            </div>
-          </div>
+          <Drawer />
         </nav>
       </header>
     </>
---
+++ src/layout/headerNav/drawer.tsx
@@ -0,0 +1,52 @@
+import { useDispatch } from 'react-redux';
+import state from '@state/ducks/index';
+import { MdClose } from 'react-icons/md';
+
+import PageList from '@layout/headerNav/pageList';
+import SocialIcon from '@component/socialIcon';
+
+import styles from '@styles/project/_p-header.module.scss';
+import headingStyles from '@styles/component/_c-heading.module.scss';
+
+const Drawer = () => {
+  const handleClose = () => dispatch(drawerState.drawerOperations.close());
+  const dispatch = useDispatch();
+  const drawerState = state.drawerState;
+  return (
+    <>
+      <div
+        className={
+          styles.drawerBack +
+          ' ' +
+          styles[drawerState.drawerSelectors.drawerSelector()]
+        }
+        onClick={() => handleClose()}
+      ></div>
+      <div
+        className={
+          styles.navDrawer +
+          ' ' +
+          styles[drawerState.drawerSelectors.drawerSelector()]
+        }
+      >
+        <span className={styles.navIcon}>
+          <MdClose onClick={() => handleClose()} />
+        </span>
+        <h2 className={styles.heading + ' ' + headingStyles.flexCenter}>
+          ページ一覧
+        </h2>
+        <div className={styles.pageList}>
+          <PageList />
+        </div>
+        <h2 className={styles.heading + ' ' + headingStyles.flexCenter}>
+          連絡先
+        </h2>
+        <div className={styles.socialIcon}>
+          <SocialIcon />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Drawer;
