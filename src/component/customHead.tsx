abf96beae56bb1c1eb0f2098d5edd2e7c5447159
sake <sakelog.website@gmail.com>
2021-01-31 20:25:56 +0900

    PWA Fixes #186

--- src/component/customHead.tsx
+++ src/component/customHead.tsx
@@ -56,19 +56,19 @@ const CustomHead: React.FC<propsType> = (props) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/img/ico/apple-touch-icon.png"
+          href="/img/icons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/img/ico/favicon-32x32.png"
+          href="/img/icons/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/img/ico/favicon-16x16.png"
+          href="/img/icons/favicon-16x16.png"
         />
         <link rel="manifest" href="/manifest.json" />
       </Head>
