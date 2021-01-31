f071372d77414d87dfc83334c3acd6fc0391120c
sake <sakelog.website@gmail.com>
2021-01-31 20:06:24 +0900

    サイト説明修正

--- src/component/config.tsx
+++ src/component/config.tsx
@@ -1,8 +1,7 @@
 export default {
   title: 'sakelog',
   author: 'sake',
-  description:
-    'WEB系の技術を中心にした技術メモ置き場です。GatsbyJS・git・TypeScript',
+  description: 'WEB系の技術を中心にした技術メモ置き場です。React,TypeScript',
   url: 'https://sake-log.website/',
   createAt: '2020-1-12',
   //GTM_ID: 'GTM-N9SLS2F',
