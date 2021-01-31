ae182887125300151491e3f303e0f7cadc0c4ee3
sake <sakelog.website@gmail.com>
2021-01-31 22:01:44 +0900

    offline設定修正

--- next.config.js
+++ next.config.js
@@ -1,14 +1,36 @@
-const withPWA = require('next-pwa');
 const { createSecureHeaders } = require('next-secure-headers');
+const withOffline = require('next-offline');
 
-module.exports = withPWA({
+module.exports = withOffline({
   images: {
     domains: ['images.ctfassets.net'],
   },
-  pwa: {
-    dest: 'public',
-  },
   async headers() {
     return [{ source: '/(.*)', headers: createSecureHeaders() }];
+  },
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? 'service-worker.js'
+      : 'public/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/service-worker.js',
+        destination: '/_next/static/service-worker.js',
+      },
+    ];
   },
 });
