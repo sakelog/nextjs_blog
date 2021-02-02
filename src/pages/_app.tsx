4ccdbf5a577f835cfb5c18c922a975c75cec93d7
sake <sakelog.website@gmail.com>
2021-02-02 22:34:49 +0900

    Algolia導入 #189

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '@styles/mystyle.scss';
 import App, { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import createStore from '@state/store';
-
+import { InstantSearch } from 'react-instantsearch-dom';
+import algoliasearch from 'algoliasearch/lite';
 class MyApp extends App {
   render(): JSX.Element {
     const { Component, pageProps }: AppProps = this.props;
     return (
       <Provider store={createStore()}>
-        <Component {...pageProps} />
+        <InstantSearch indexName="post" searchClient={searchClient}>
+          <Component {...pageProps} />
+        </InstantSearch>
       </Provider>
     );
   }
