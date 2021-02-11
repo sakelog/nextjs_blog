4ba8f82674a5cb989ec16abacf83181356eadd0f
sake <sakelog.website@gmail.com>
2021-02-11 20:08:47 +0900

    bug-fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,30 @@
 import '@styles/mystyle.scss';
-import App, { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import createStore from '@state/store';
 import { InstantSearch } from 'react-instantsearch-dom';
 import { indexName, searchClient } from '@lib/algolia/client';
-class MyApp extends App {
-  render(): JSX.Element {
-    const { Component, pageProps }: AppProps = this.props;
-    return (
-      <Provider store={createStore()}>
-        <InstantSearch indexName={indexName} searchClient={searchClient}>
+import { ThemeProvider } from '@material-ui/core/styles';
+import { muiTheme } from '@lib/mui/theme';
+
+const MyApp = (props: AppProps) => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    jssStyles &&
+      jssStyles.parentElement &&
+      jssStyles.parentElement.removeChild(jssStyles);
+  }, []);
+  const { Component, pageProps }: AppProps = props;
+  return (
+    <Provider store={createStore()}>
+      <InstantSearch indexName={indexName} searchClient={searchClient}>
+        <ThemeProvider theme={muiTheme}>
           <Component {...pageProps} />
-        </InstantSearch>
-      </Provider>
-    );
-  }
-}
+        </ThemeProvider>
+      </InstantSearch>
+    </Provider>
+  );
+};
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import React from 'react';
 class MyDocument extends Document {
   render(): JSX.Element {
     const GTMNoScript =
@@ -19,5 +21,29 @@ class MyDocument extends Document {
     );
   }
 }
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
 
 export default MyDocument;
