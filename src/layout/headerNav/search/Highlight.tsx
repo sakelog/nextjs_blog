ca397cb67431b5386269ec2f40f8c82f01bbb283
sake <sakelog.website@gmail.com>
2021-02-11 16:44:27 +0900

    検索結果デザイン修正

--- src/layout/headerNav/search/Highlight.tsx
+++ src/layout/headerNav/search/Highlight.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { HighlightProps } from 'react-instantsearch-core';
 import { connectHighlight } from 'react-instantsearch-dom';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { Message } from './types';
+import config from '@component/config';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    span: {
+      color: '#333333',
+    },
+    mark: {
+      background: config.themeColor,
+      color: '#ffffff',
+      padding: theme.spacing(0, 1),
+      margin: theme.spacing(1),
+    },
+  })
+);
 
 const HighlightHit: React.FC<HighlightProps<Message>> = ({
   hit,
@@ -13,13 +29,18 @@ const HighlightHit: React.FC<HighlightProps<Message>> = ({
     attribute,
     hit,
   });
+  const styles = useStyles();
   return (
     <>
       {parsedHit.map((part, index) =>
         part.isHighlighted ? (
-          <mark key={index}>{part.value}</mark>
+          <mark key={index} className={styles.mark}>
+            {part.value}
+          </mark>
         ) : (
-          <span key={index}>{part.value}</span>
+          <span key={index} className={styles.span}>
+            {part.value}
+          </span>
         )
       )}
     </>
--- src/layout/headerNav/search/search.tsx
+++ src/layout/headerNav/search/search.tsx
@@ -1,36 +1,119 @@
+import Link from 'next/link';
 import { connectAutoComplete } from 'react-instantsearch-dom';
 import { AutocompleteProvided } from 'react-instantsearch-core';
+import { InputBase, List, ListItem } from '@material-ui/core';
+import {
+  createStyles,
+  fade,
+  Theme,
+  makeStyles,
+} from '@material-ui/core/styles';
 import { MdSearch } from 'react-icons/md';
 import Highlight from './Highlight';
 import { Message } from './types';
 
 import { getRootPath } from '@lib/getPath';
-import styles from '@styles/component/_c-search.module.scss';
+import config from '@component/config';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'relative',
+      flexGrow: 1,
+      flexShrink: 0,
+      flexBasis: '30%',
+      margin: theme.spacing(0, 2),
+    },
+    search: {
+      position: 'relative',
+      borderRadius: '6px',
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+      width: '100%',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+    },
+    hits: {
+      position: 'fixed',
+      background: '#ffffff',
+      zIndex: 999,
+      left: 0,
+      width: '100%',
+      padding: theme.spacing(2),
+      boxShadow: '0 2px 5px rgba(0, 0, 0, 0.26)',
+    },
+    hit: {
+      borderBottom: '1px dashed #000000',
+    },
+    clearWrapper: {
+      textAlign: 'right',
+    },
+    clear: {
+      background: config.themeColor,
+      color: '#ffffff',
+      fontWeight: 'bold',
+      padding: theme.spacing(1),
+      cursor: 'pointer',
+    },
+  })
+);
 
 type Props = AutocompleteProvided<Message>;
 
 const Autocomplete: React.FC<Props> = ({ hits, currentRefinement, refine }) => {
+  const styles = useStyles();
   return (
     <div className={styles.root}>
-      <div className={styles.searchBox}>
-        <MdSearch className={styles.searchIcon} />
-        <input
-          type="search"
-          placeholder="検索"
+      <div className={styles.search}>
+        <div className={styles.searchIcon}>
+          <MdSearch />
+        </div>
+        <InputBase
+          placeholder="検索…"
+          classes={{
+            root: styles.inputRoot,
+            input: styles.inputInput,
+          }}
+          inputProps={{ 'aria-label': '検索' }}
           value={currentRefinement}
           onChange={(event) => refine(event.currentTarget.value)}
         />
       </div>
       {currentRefinement != '' && (
-        <ul className={styles.hits}>
-          {hits.map((hit) => (
-            <li key={hit.objectID}>
-              <a href={getRootPath(hit.slug.ja).slice(0, -1)}>
-                <Highlight hit={hit} attribute="title.ja" />
-              </a>
-            </li>
-          ))}
-        </ul>
+        <div className={styles.hits} onClick={() => refine()}>
+          <List>
+            {hits.map((hit) => (
+              <ListItem key={hit.objectID} button className={styles.hit}>
+                <Link href={getRootPath(hit.slug.ja)}>
+                  <Highlight hit={hit} attribute="title.ja" />
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+          <div className={styles.clearWrapper}>
+            <span className={styles.clear} onClick={() => refine()}>
+              クリア
+            </span>
+          </div>
+        </div>
       )}
     </div>
   );
