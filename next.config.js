95f94db017424e3a87f3b14c5ba31cb785249554
sake <sakelog.website@gmail.com>
2021-02-13 10:27:32 +0900

    nextconfig修正

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
-const { createSecureHeaders } = require('next-secure-headers');
-const { withPlugins, optional } = require('next-compose-plugins');
+const withPWA = require('next-pwa');
 
 const nextConfig = {
   images: {
     domains: ['images.ctfassets.net'],
-  },
-  async headers() {
-    return [{ source: '/(.*)', headers: createSecureHeaders() }];
   },
   pwa: {
     dest: 'public',
   },
 };
 
-module.exports = withPlugins(
-  [[optional(() => require('next-pwa')), {}], [createSecureHeaders]],
-  nextConfig
-);
+module.exports = withPWA(nextConfig);
