c0588bd62e2d7ff5a6334105023b0912238d791d
sake <sakelog.website@gmail.com>
2021-02-13 21:55:58 +0900

    mediaQuery削除

--- src/component/config.tsx
+++ src/component/config.tsx
@@ -4,16 +4,9 @@ export default {
   description: 'WEB系の技術を中心にした技術メモ置き場です。React,TypeScript',
   url: 'https://sake-log.website/',
   createAt: '2020-1-12',
-  //GTM_ID: 'GTM-N9SLS2F',
   social: {
     twitter: 'sake_engineer',
     github: 'sakelog',
-  },
-  mediaQuery: {
-    xs: 599,
-    sm: 959,
-    md: 1279,
-    lg: 1919,
   },
   themeColor: '#5b7e91',
 };
