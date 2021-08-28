ceb0562b731f1e439f13acb42c360e97ccfb0bd7
sake <sakelog.website@gmail.com>
2021-08-29 07:02:07 +0900

    GTM遅延読み込み

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
     const GTMScript = () => {
       return process.env.NODE_ENV === 'production' ? (
         <script
+          async
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
