e8d1eff6d9ac5c3b05866738cd1c957beaacf6db
sake <sakelog.website@gmail.com>
2021-09-16 21:37:36 +0900

    markdown処理分割

---
+++ src/components/markdownBody.tsx
@@ -0,0 +1,23 @@
+import type {
+  JSXElementConstructor,
+  ReactElement,
+} from 'react';
+
+import { markdownRenderer } from '@lib/markdownRenderer';
+
+type PropsType = {
+  markdown: string;
+};
+
+const renderMarkdownBody = (
+  props: PropsType
+): ReactElement => {
+  const parsedMarkdown = markdownRenderer.processSync(
+    props.markdown
+  );
+
+  const result: ReactElement | any = parsedMarkdown.result;
+  return result;
+};
+
+export default renderMarkdownBody;
