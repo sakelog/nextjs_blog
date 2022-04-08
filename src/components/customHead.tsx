d2c8152192bc7722bf8ca5bc7822423d9f00aa59
sake <sakelog.website@gmail.com>
2022-04-09 02:00:15 +0900

    react.fc削除

--- src/components/customHead.tsx
+++ src/components/customHead.tsx
@@ -8,7 +8,7 @@ type propsType = {
   imgFLG?: boolean;
 };
 
-const CustomHead: React.FC<propsType> = (props) => {
+const CustomHead = (props: propsType) => {
   const metaTitle = props.pageTitle
     ? props.pageTitle + ' | ' + config.title
     : config.title;
@@ -22,75 +22,67 @@ const CustomHead: React.FC<propsType> = (props) => {
       '/v1611679454/sakelog/postOGP.png'
     : 'https://res.cloudinary.com/cl1sakelog/image/upload/v1611678550/sakelog/defaultOGP.png';
   return (
-    <>
-      <Head>
-        <title>{metaTitle}</title>
-        <meta
-          name="description"
-          content={metaDiscription}
-        />
-        <meta property="og:title" content={metaTitle} />
-        <meta
-          property="og:description"
-          content={metaDiscription}
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content={ogpImageSrc} />
-        <meta
-          property="twitter:card"
-          content="summary_large_image"
-        />
-        <meta
-          property="twitter:title"
-          content={metaTitle}
-        />
-        <meta
-          property="twitter:description"
-          content={metaDiscription}
-        />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <meta
-          name="theme-color"
-          content={config.themeColor}
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preconnect"
-          href="https://www.googletagmanager.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/icons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/img/icons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/img/icons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-    </>
+    <Head>
+      <title>{metaTitle}</title>
+      <meta name="description" content={metaDiscription} />
+      <meta property="og:title" content={metaTitle} />
+      <meta
+        property="og:description"
+        content={metaDiscription}
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={ogpImageSrc} />
+      <meta
+        property="twitter:card"
+        content="summary_large_image"
+      />
+      <meta property="twitter:title" content={metaTitle} />
+      <meta
+        property="twitter:description"
+        content={metaDiscription}
+      />
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+      <meta
+        name="theme-color"
+        content={config.themeColor}
+      />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preconnect"
+        href="https://www.googletagmanager.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/img/icons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/img/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/img/icons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
   );
 };
 
