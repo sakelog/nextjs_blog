019aef0d8e65dca8c475b7ad6830d3809c253d93
sake <sakelog.website@gmail.com>
2022-04-18 08:39:46 +0900

    next-pwa無効化

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-
 const nextConfig = {
   images: {
     domains: ['images.ctfassets.net'],
   },
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
   reactStrictMode: true,
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
