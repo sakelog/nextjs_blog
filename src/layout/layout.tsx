f2e74ef4772c6bfe4c45c70813070bce6c820b6b
sake <sakelog.website@gmail.com>
2022-04-18 03:53:14 +0900

    adsense設定修正

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,11 +1,27 @@
 import Header from '@layout/_header';
 import Footer from '@layout/_footer';
+import { useEffect } from 'react';
 
 const Layout = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push(
+        {}
+      );
+    } catch (err) {
+      console.log(err);
+    }
+    return () => {
+      const main = document.querySelector(
+        'main'
+      ) as HTMLElement;
+      main.removeAttribute('style');
+    };
+  }, []);
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Header />
