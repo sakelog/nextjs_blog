b1b2f0eb09d17f25747e5e7964e6bbe3374a9bcc
sake <sakelog.website@gmail.com>
2022-05-07 15:57:13 +0900

    indexList修正

--- src/components/IndexList.tsx
+++ src/components/IndexList.tsx
@@ -12,27 +12,38 @@ type PropsType = {
   posts: Contentful.Post[] | null;
 };
 
-const IndexList = (props: PropsType) => (
+const IndexList = ({ posts }: PropsType) => (
   <div>
     <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      {props.posts &&
-        props.posts.map((post) => {
+      {posts &&
+        posts.map((post) => {
           return (
-            <li key={post.fields.slug}>
-              <PostDate
-                postdate={post.fields.date}
-                update={post.fields.update}
-              />
-              <Link href={getPostPath(post.fields.slug)}>
-                <a className="text-xl">
-                  <h2>{post.fields.title}</h2>
-                </a>
-              </Link>
-              <div className="my-2">
-                <TagList
-                  tags={post.fields.tags}
-                  heading="h4"
+            <li
+              key={post.fields.slug}
+              className="border-2 border-gray-400 relative mt-12 pt-12 p-4 rounded-md"
+            >
+              <div className="absolute -top-12 left-1 bg-white p-2">
+                <PostDate
+                  postdate={post.fields.date}
+                  update={post.fields.update}
                 />
+              </div>
+              <div className="h-full flex flex-col">
+                <div className="flex-1 p-2 border-b border-gray-400">
+                  <Link
+                    href={getPostPath(post.fields.slug)}
+                  >
+                    <a className="text-xl">
+                      <h2>{post.fields.title}</h2>
+                    </a>
+                  </Link>
+                </div>
+                <div className="pt-2">
+                  <TagList
+                    tags={post.fields.tags}
+                    heading="h4"
+                  />
+                </div>
               </div>
             </li>
           );
